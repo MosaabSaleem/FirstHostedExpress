839cdc217621ea1ae6c92c14ce78fcd2dd2ac4a3
MosaabSaleem <mmosaab2002@hotmail.com>
2025-07-30 16:29:32 +1000

    Express shows react homepage

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Example API route
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Express!' });
+});
+
+// Serve React for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on PORT:${port}`);
+  console.log(`Server running on port ${port}`);
 });
-
